76b75d3f121f7e287df7267b01a67cc178ec0e9f
qquinton <jumazquinton.jq@gmail.com>
2026-04-06 12:45:52 +0300

    Relocate main.go file

---
+++ cmd/server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world. Time to connect with the most credible health care all at your convenience. Just sit back and let the doctor take good care of you.")
+}
